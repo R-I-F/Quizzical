e8e173e6f169e9f1b897b30fcdc91c6a2be9278f
R-I-F <ibrahimrefaeei@gmail.com>
2023-09-20 11:49:01 +0300

    Initial Commit

--- src/Startpage.jsx
+++ src/Startpage.jsx
@@ -2,7 +2,7 @@ export default function Startpage(props){
     return (
         <div className="start-page">
             <h1>Quizzical</h1>
-            <p>Some description if needed</p>
+            <p>Quiz Time: Test Your Knowledge!</p>
             <button 
             className="btn-one"
             onClick= {props.handleStartbtn}
